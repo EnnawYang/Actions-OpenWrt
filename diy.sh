aad224b9dd5982779e361c444b1041016517be2a
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2019-12-01 20:40:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,12 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Mrbai98/luci-theme-atmaterial package/luci-theme-atmaterial
+
+# atmaterial主题
+git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-theme-atmaterial
+
+# Server酱-微信提醒
+git clone https://github.com/tty228/luci-app-serverchan package/mine/luci-app-serverchan
+
+# App过滤
+git clone https://github.com/destan19/OpenAppFilter package/mine/OpenAppFilter
