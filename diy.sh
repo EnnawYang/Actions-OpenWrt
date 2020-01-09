8ddd00145c1fe90710a88e2f7b7c2ae42665942f
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2020-01-09 10:25:32 +0800

    cancel Docker

--- diy.sh
+++ diy.sh
@@ -56,8 +56,8 @@ git clone https://github.com/lisaac/luci-app-diskman package/mine/luci-app-diskm
 mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
 
 # Docker Manager interface for LuCI + Docker Engine API for LuCI
-git clone https://github.com/lisaac/luci-lib-docker package/mine/luci-lib-docker
-git clone https://github.com/lisaac/luci-app-dockerman package/mine/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-lib-docker package/mine/luci-lib-docker
+#git clone https://github.com/lisaac/luci-app-dockerman package/mine/luci-app-dockerman
 
 # Lienol-package
 git clone https://github.com/Lienol/openwrt-package package/Lienol-package
