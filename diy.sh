43925cfcc55497920c2ef6853bfe2bedbbceaea5
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2020-02-07 09:38:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 #=================================================
 
 # 修改机器名称
-sed -i 's/OpenWrt/OpenWrt_x86/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenWrt_x86/g' package/base-files/files/bin/config_generate
 
 # 修改网址
 #sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
@@ -22,10 +22,10 @@ sed -i 's/OpenWrt/OpenWrt_x86/g' package/base-files/files/bin/config_generate
 #sed -i 's/OpenWrt/OpenWrt_x86/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 防火墙wan口入站数据修改成ACCEPT(第21行)。如您的网络是公网，建议注释掉此项。
-sed -i '21s/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+#sed -i '21s/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
 
 # 允许远程主机连接到本地 SSH 转发端口。如您的网络是公网，建议注释掉此项。
-sed -i "4a\ \toption GatewayPorts 'on'" package/network/services/dropbear/files/dropbear.config
+#sed -i "4a\ \toption GatewayPorts 'on'" package/network/services/dropbear/files/dropbear.config
 
 # atmaterial主题
 git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-theme-atmaterial
