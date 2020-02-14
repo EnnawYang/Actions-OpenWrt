e530b549d31b73561e22cf702dfb17fce2d7fdf1
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2020-02-14 12:09:32 +0800

    cancel Lienol-package

--- diy.sh
+++ diy.sh
@@ -60,21 +60,21 @@ mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makef
 #git clone https://github.com/lisaac/luci-app-dockerman package/mine/luci-app-dockerman
 
 # Lienol-package
-git clone https://github.com/Lienol/openwrt-package package/Lienol-package
-# https://github.com/Lienol/openwrt-package/issues/54
-cp -r package/Lienol-package/lienol/luci-app-fileassistant package/mine/luci-app-fileassistant
-cp -r package/Lienol-package/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
-cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
-cp -r package/Lienol-package/lienol/luci-theme-argon-dark-mod package/mine/luci-theme-argon-dark-mod
-cp -r package/Lienol-package/lienol/luci-theme-argon-light-mod package/mine/luci-theme-argon-light-mod
-#cp -r package/Lienol-package/lienol/luci-theme-bootstrap-mod package/mine/luci-theme-bootstrap-mod
-cp -r package/Lienol-package/lienol/luci-theme-netgear-mc package/mine/luci-theme-netgear-mc
-#cp package/Lienol-package/lienol/luci_my.mk package/mine
-cp -r package/Lienol-package/package/brook package/mine/brook
-cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
-cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
-cp -r package/Lienol-package/package/tcping package/mine/tcping
-rm -rf package/Lienol-package
+#git clone https://github.com/Lienol/openwrt-package package/Lienol-package
+## https://github.com/Lienol/openwrt-package/issues/54
+#cp -r package/Lienol-package/lienol/luci-app-fileassistant package/mine/luci-app-fileassistant
+#cp -r package/Lienol-package/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
+#cp -r package/Lienol-package/lienol/luci-app-passwall package/mine/luci-app-passwall
+#cp -r package/Lienol-package/lienol/luci-theme-argon-dark-mod package/mine/luci-theme-argon-dark-mod
+#cp -r package/Lienol-package/lienol/luci-theme-argon-light-mod package/mine/luci-theme-argon-light-mod
+##cp -r package/Lienol-package/lienol/luci-theme-bootstrap-mod package/mine/luci-theme-bootstrap-mod
+#cp -r package/Lienol-package/lienol/luci-theme-netgear-mc package/mine/luci-theme-netgear-mc
+##cp package/Lienol-package/lienol/luci_my.mk package/mine
+#cp -r package/Lienol-package/package/brook package/mine/brook
+#cp -r package/Lienol-package/package/chinadns-ng package/mine/chinadns-ng
+#cp -r package/Lienol-package/package/dns2socks package/mine/dns2socks
+#cp -r package/Lienol-package/package/tcping package/mine/tcping
+#rm -rf package/Lienol-package
 
 # luci-app-vssr
 #git clone https://github.com/jerrykuku/luci-app-vssr package/mine/luci-app-vssr
