61a56ffdc956bfc2ba40ebf8b08ef9d7089cb589
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2020-04-25 11:01:50 +0800

    Merge

--- newifi-d2_part1.sh
+++ newifi-d2_part1.sh
@@ -20,20 +20,8 @@ git clone https://github.com/Mrbai98/luci-theme-atmaterial package/mine/luci-the
 # Rosy Theme for LuCI [ OpenWrt-18.06 Branch Only ]
 git clone https://github.com/rosywrt/luci-theme-rosy package/mine/rosy
 
-# Serverchan
-git clone https://github.com/tty228/luci-app-serverchan package/mine/luci-app-serverchan
-# 修改读取温度文件为/sys/class/hwmon/hwmon1/temp*_input
-# sed -i 's/\/sys\/class\/thermal\/thermal_zone\*\/temp/\/sys\/class\/hwmon\/hwmon1\/temp\*_input/g' package/mine/luci-app-serverchan/root/usr/bin/serverchan/serverchan
-
 # luci-app-ssr-plus
 git clone https://github.com/fw876/helloworld package/mine/luci-app-ssr-plus
-
-# OpenAppFilter(luci-app-oaf)
-# git clone https://github.com/destan19/OpenAppFilter package/mine/OpenAppFilter
-
-# DiskMan for LuCI (WIP)
-# git clone https://github.com/lisaac/luci-app-diskman package/mine/luci-app-diskman
-# mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/mine/parted/Makefile
 
 # svn Lienol's apps & themes，https://github.com/Lienol/openwrt-package
 # svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
--- newifi-d2_part2.sh
+++ newifi-d2_part2.sh
@@ -12,7 +12,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # 修改机器名称
-sed -i 's/OpenWrt/OpenWrt_x86/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/newifi-d2/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题
 #sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
