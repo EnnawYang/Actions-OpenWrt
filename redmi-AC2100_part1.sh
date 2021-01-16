2a1d09aac1e8c6d59ac0233cecd75731458d67d9
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2021-01-16 20:52:14 +0800

    add Redmi-AC2100 config

---
+++ redmi-AC2100_part1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# atmaterial主题
+# git clone https://github.com/yangsongli/luci-theme-atmaterial package/mine/luci-theme-atmaterial
+
+# Rosy Theme for LuCI [ OpenWrt-18.06 Branch Only ]
+# git clone https://github.com/rosywrt/luci-theme-rosy package/mine/rosy
+
+# luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld package/mine/luci-app-ssr-plus
+
+# svn Lienol's apps & themes，https://github.com/Lienol/openwrt-package
+# svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-filebrowser package/mine/luci-app-filebrowser
---
+++ redmi-AC2100_part2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 修改机器名称
+sed -i 's/OpenWrt/Redmi-AC2100/g' package/base-files/files/bin/config_generate
+
+# 更改默认主题
+#sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# 防火墙wan口入站数据修改成ACCEPT(第21行)。如您的网络是公网，建议注释掉此项。
+#sed -i '21s/REJECT/ACCEPT/g' package/network/config/firewall/files/firewall.config
+
+# 允许远程主机连接到本地 SSH 转发端口。如您的网络是公网，建议注释掉此项。
+#sed -i "4a\ \toption GatewayPorts 'on'" package/network/services/dropbear/files/dropbear.config
