fe645339cc6e3dd6d6a8e132d75f05aa7acab39b
EnnawYang <6705663+EnnawYang@users.noreply.github.com>
2021-03-09 18:27:42 +0800

    -

--- pi4b-part2.sh
+++ pi4b-part2.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-rm -rf package/mine/
-rm -rf dl/*-cache/
-
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
